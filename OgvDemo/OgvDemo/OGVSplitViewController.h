4d7de00843ad19a72a9712a23d97bc7d7b9e49fb
Brion Vibber <brion@pobox.com>
2014-06-28 05:08:20 -0700

    Fix split view controller to stay split in portrait

---
+++ OgvDemo/OgvDemo/OGVSplitViewController.h
@@ -0,0 +1,13 @@
+//
+//  OGVSplitViewController.h
+//  OgvDemo
+//
+//  Created by Brion on 6/28/14.
+//  Copyright (c) 2014 Brion Vibber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OGVSplitViewController : UISplitViewController <UISplitViewControllerDelegate>
+
+@end
---
+++ OgvDemo/OgvDemo/OGVSplitViewController.m
@@ -0,0 +1,57 @@
+//
+//  OGVSplitViewController.m
+//  OgvDemo
+//
+//  Created by Brion on 6/28/14.
+//  Copyright (c) 2014 Brion Vibber. All rights reserved.
+//
+
+#import "OGVSplitViewController.h"
+
+@interface OGVSplitViewController ()
+
+@end
+
+@implementation OGVSplitViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.delegate = self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+#pragma mark - delegate methods
+
+- (BOOL)splitViewController:(UISplitViewController *)svc shouldHideViewController:(UIViewController *)vc inOrientation:(UIInterfaceOrientation)orientation
+{
+    return NO;
+}
+
+@end
